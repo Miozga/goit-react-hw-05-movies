3dd5dc85b53cc2cc8f047e65e40fdde861f47432
Sebastian Miozga <miozga.sebastian@gmail.com>
2024-05-25 12:22:44 +0200

    123

---
+++ src/components/Cast.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieCredits } from '../../services/api';
+
+const Cast = () => {
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+
+  useEffect(() => {
+    getMovieCredits(movieId).then(data => setCast(data.cast));
+  }, [movieId]);
+
+  return (
+    <div>
+      <h2>Cast</h2>
+      <ul>
+        {cast.map(actor => (
+          <li key={actor.id}>{actor.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Cast;
